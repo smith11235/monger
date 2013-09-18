fa051f0664f4db3b8b3a0f1f7c2dc3bfa85567d9
smith11235 <smith11235@gmail.com>
2013-09-17 20:38:44 -0400

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monger::Application.config.secret_key_base = '98a166621660f983fc3d260f108270a976a7a061e3c0102a65afa1f32126911c059629c87cad1083c042a056797f581ac4658421ff1998d41fd552b8f21ce9f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Monger::Application.config.session_store :cookie_store, key: '_monger_session'
